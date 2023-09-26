c43e16e25af097404cb44bfe946379943d053ead
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2023-09-26 08:20:33 +0200

    Adding page and menu items

---
+++ app/content/page.tsx
@@ -0,0 +1,20 @@
+// Auth
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+
+// Components
+import { redirect } from "next/navigation"
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect("/")
+  }
+
+  return (
+    <div className="grid gap-4 items-center justify-center m-20 text-4xl">
+      <h1>Content</h1>
+    </div>
+  )
+}
