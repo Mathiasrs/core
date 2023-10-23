f9db8039b71c118724e7a0d562a6397b200bb76d
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2023-10-23 17:07:18 +0200

    Removing Form from
    To be ready for the new EditUserSheet implementation, it was necessary to remove old code, which was intended for the CreateContentSheet.

--- components/EditUserSheet.tsx
+++ components/EditUserSheet.tsx
@@ -63,79 +63,12 @@ export default function EditUserSheet() {
     <Sheet open={isOpen?.isOpen} onOpenChange={() => setIsOpen()}>
       <SheetContent className="w-full sm:w-[34rem]">
         <SheetHeader>
-          <SheetTitle>Create new content</SheetTitle>
+          <SheetTitle>Edit user</SheetTitle>
           <SheetDescription>
-            The first word is the hardest, but it unlocks a world of
-            possibilities. Ready, set, create!
+            Below you can edit the user and provide the necessary user
+            permissions.
           </SheetDescription>
         </SheetHeader>
-
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="mt-6 grid gap-6"
-          >
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Add title" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    The title should be descriptive for easy content management.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="Add description" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    A description helps the user to quickly identify content. It
-                    is of nature optional.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="contentId"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Content ID</FormLabel>
-                  <FormControl>
-                    <Input placeholder="HT05102301" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    A content ID will help structure and manage content. It is a
-                    unique set of characters and numbers.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <div className="absolute bottom-0 left-0 w-full p-4 pb-4">
-              <Button type="submit" className="flex w-full gap-2">
-                <CheckIcon />
-                <span>Create now</span>
-              </Button>
-            </div>
-          </form>
-        </Form>
       </SheetContent>
     </Sheet>
   )
