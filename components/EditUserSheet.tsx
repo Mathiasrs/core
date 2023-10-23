23c1803b5ee970d20c4e458218dd433e0d249446
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2023-10-23 17:08:11 +0200

    Update EditUserSheet.tsx

--- components/EditUserSheet.tsx
+++ components/EditUserSheet.tsx
@@ -1,19 +1,5 @@
 "use client"
 
-import { useState } from "react"
-
-// Mutations
-import { useCreateContent } from "@/app/actions/mutations/content/useCreateContent"
-
-// Libraries
-import { CheckIcon, PlusIcon } from "@radix-ui/react-icons"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-
-// Helpers
-import { generateSlug } from "@/lib/helpers/generateSlug"
-
 // Components
 import {
   Sheet,
@@ -21,43 +7,12 @@ import {
   SheetDescription,
   SheetHeader,
   SheetTitle,
-  SheetTrigger,
 } from "@/components/ui/sheet"
-import { Button } from "@/components/ui/button"
 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "./ui/input"
-import { Textarea } from "./ui/textarea"
-import { ContentCreationValidator } from "@/lib/validators/content"
 import { useToggleEditUserSheet } from "@/app/actions/states/useToggleEditUserSheet"
 
 export default function EditUserSheet() {
   const { isOpen, setIsOpen } = useToggleEditUserSheet()
-  const [isMutating, setIsMutating] = useState(false)
-
-  const form = useForm<z.infer<typeof ContentCreationValidator>>({
-    resolver: zodResolver(ContentCreationValidator),
-  })
-
-  const createContent = useCreateContent(setIsOpen, setIsMutating)
-
-  function onSubmit(data: z.infer<typeof ContentCreationValidator>) {
-    const payload = {
-      contentId: data.contentId,
-      title: data.title,
-      slug: generateSlug(data.title),
-      description: data.description,
-    }
-    createContent.mutate(payload)
-  }
 
   return (
     <Sheet open={isOpen?.isOpen} onOpenChange={() => setIsOpen()}>
