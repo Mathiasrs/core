f4abc2d7d03412c240622aed84a1302e10e361d8
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2023-11-02 09:56:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "avatars.slack-edge.com"],
+    domains: [
+      "res.cloudinary.com",
+      "avatars.slack-edge.com",
+      process.env.VERCEL_BLOB_STORAGE_URL,
+    ],
   },
 }
 
