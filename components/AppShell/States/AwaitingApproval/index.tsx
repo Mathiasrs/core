9926475115b1d4fbbfef681d2bb0c741fa74c141
Mathias Riis Sørensen <72079659+mathiasrscom@users.noreply.github.com>
2023-11-06 19:08:50 +0100

    Adding Message component
    In preparation for adding messages to pages not available, a message component was create to limit repeated code.

--- components/AppShell/States/AwaitingApproval/index.tsx
+++ components/AppShell/States/AwaitingApproval/index.tsx
@@ -1,4 +1,5 @@
 // Auth
+import Message from "@/components/Message"
 import { Button } from "@/components/ui/button"
 import { signOut } from "next-auth/react"
 
@@ -9,19 +10,12 @@ import { FaArrowRightFromBracket } from "react-icons/fa6"
 
 export default function AwaitingApproval() {
   return (
-    <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8 mt-40">
-      <div className="text-center">
-        <h2 className="text-base font-semibold uppercase tracking-wide text-green-600">
-          User activity
-        </h2>
-        <p className="mt-1 text-4xl font-extrabold text-zinc-900 dark:text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
-          Awaiting your manager approval.
-        </p>
-        <p className="mx-auto mt-5 max-w-xl text-xl text-zinc-500 dark:text-zinc-400">
-          Your account is awaiting your manager to accept and asign you the relevant user
-          permissions.
-        </p>
-      </div>
+    <div className="mx-auto mt-40 max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+      <Message
+        category="User actitivty"
+        title="Awaiting your manager approval."
+        description="Your account is awaiting your manager to accept and asign you the relevant user permissions."
+      />
 
       <div className="m-auto grid max-w-lg gap-2 pt-6 sm:flex sm:justify-center md:pt-8">
         <Button onClick={() => signOut()}>
